48220fa831a670773ab8c521bbec01b3e5a91c18
Malachi Soord <inverse.chi@gmail.com>
2020-10-15 22:52:09 +0200

    typehintg

--- libratone/client.py
+++ libratone/client.py
@@ -4,7 +4,7 @@ import logging
 UDP_PORT = 7777
 
 
-def send_command(device_ip: str, command):
+def send_command(device_ip: str, command: bytes):
     logging.info(f'Sending to {device_ip} command: {command}')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(command, (device_ip, UDP_PORT))
