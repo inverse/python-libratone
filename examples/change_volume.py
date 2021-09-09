a8ef2cd0f36bc5fcd5a5670d2e1245fd6730c7bf
Malachi Soord <inverse.chi@gmail.com>
2021-09-09 10:35:32 +0200

    Improve linting (#11)

--- examples/change_volume.py
+++ examples/change_volume.py
@@ -5,9 +5,9 @@ import sys
 from libratone.client import send_command
 from libratone.commands import get_volume
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print('You must provide <ip-address> <volume-level>')
+        print("You must provide <ip-address> <volume-level>")
         exit(1)
 
     logging.basicConfig(level=logging.DEBUG)
--- libratone/client.py
+++ libratone/client.py
@@ -5,6 +5,6 @@ UDP_PORT = 7777
 
 
 def send_command(device_ip: str, command: bytes):
-    logging.info(f'Sending to {device_ip} command: {command}')
+    logging.info(f"Sending to {device_ip} command: {command.decode()}")
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(command, (device_ip, UDP_PORT))
