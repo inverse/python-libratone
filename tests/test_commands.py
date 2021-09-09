3d7f7a94d7322e49071eebde64e49a40cd507805
Malachi Soord <inverse.chi@gmail.com>
2021-09-09 10:34:30 +0200

    Improve linting

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,11 +4,16 @@ from libratone.commands import get_volume
 
 
 class VolumeTests(unittest.TestCase):
-
     def test_valid_volume(self):
-        self.assertEqual(b'\x00\x00\x02\x00\x40\x00\x00\x00\x00\x02\x30\x30', get_volume(0))
-        self.assertEqual(b'\x00\x00\x02\x00\x40\x00\x00\x00\x00\x02\x35\x30', get_volume(50))
-        self.assertEqual(b'\x00\x00\x02\x00\x40\x00\x00\x00\x00\x03\x31\x30\x30', get_volume(100))
+        self.assertEqual(
+            b"\x00\x00\x02\x00\x40\x00\x00\x00\x00\x02\x30\x30", get_volume(0)
+        )
+        self.assertEqual(
+            b"\x00\x00\x02\x00\x40\x00\x00\x00\x00\x02\x35\x30", get_volume(50)
+        )
+        self.assertEqual(
+            b"\x00\x00\x02\x00\x40\x00\x00\x00\x00\x03\x31\x30\x30", get_volume(100)
+        )
 
     def test_invalid_volume(self):
         with self.assertRaises(ValueError):
